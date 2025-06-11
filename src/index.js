b3171debdf79fe695ff1984b0df7d427e7cde6dd
Patanakiat Onsiri <65020945@my.buu.ac.th>
2025-06-11 18:55:28 +0700

    fix: correct Firebase env names

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+console.table({
+  api:         process.env.REACT_APP_FB_API_KEY,
+  authDomain:  process.env.REACT_APP_FB_AUTH_DOMAIN
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
